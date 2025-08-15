2b945aa4a7ce2bd90e3e3dd093caa81ec22b57a3
Li Xiaoyu <u3637493@connect.hku.hk>
2025-08-15 17:21:43 +0800

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,5 +1,3 @@
-import { NextResponse } from 'next/server';
-
 export const config = {
   matcher: '/(.*)', // 匹配所有路径
 };
@@ -7,15 +5,17 @@ export const config = {
 export default function middleware(req) {
   const url = req.nextUrl.clone();
   const cookie = req.headers.get('cookie') || '';
+
+  // 使用环境变量SITE_PASSWORD
   const correctPassword = process.env.SITE_PASSWORD;
 
-  // 检查请求中的 cookie 是否包含正确的密码
+  // 检查请求中的cookie是否包含正确的密码
   if (!cookie.includes(`site_password=${correctPassword}`)) {
     // 如果没有密码或密码错误，重定向到登录页面
     url.pathname = '/login';
-    return NextResponse.redirect(url);
+    return Response.redirect(url);
   }
 
   // 如果密码正确，继续请求
-  return NextResponse.next();
+  return Response.next();
 }
