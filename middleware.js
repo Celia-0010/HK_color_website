f926aa4a4588d4e71e1a6ff2fad668b47f9e5a29
Li Xiaoyu <u3637493@connect.hku.hk>
2025-08-15 17:16:12 +0800

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,22 +1,22 @@
-import { NextResponse } from 'next/server';
-
+// Vercel Edge Function配置
 export const config = {
   matcher: '/(.*)', // 匹配所有路径
 };
-
-const CORRECT_PASSWORD = process.env.SITE_PASSWORD; // 使用环境变量
 
 export default function middleware(req) {
   const url = req.nextUrl.clone();
   const cookie = req.headers.get('cookie') || '';
 
-  // 检查是否访问受保护的页面且没有正确的密码 cookie
-  if (
-    !cookie.includes(`site_password=${CORRECT_PASSWORD}`)
-  ) {
-    url.pathname = '/login'; // 重定向到登录页面
-    return NextResponse.redirect(url);
+  // 使用环境变量SITE_PASSWORD
+  const correctPassword = process.env.SITE_PASSWORD;
+
+  // 检查请求中的cookie是否包含正确的密码
+  if (!cookie.includes(`site_password=${correctPassword}`)) {
+    // 如果没有密码或密码错误，重定向到登录页面
+    url.pathname = '/login';
+    return Response.redirect(url);
   }
 
-  return NextResponse.next();
+  // 如果密码正确，继续请求
+  return Response.next();
 }
