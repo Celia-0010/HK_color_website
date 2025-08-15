897435c68c0e5dcd1d2027d70c23033b70cffb1c
Li Xiaoyu <u3637493@connect.hku.hk>
2025-08-15 17:13:53 +0800

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -4,14 +4,15 @@ export const config = {
   matcher: '/(.*)', // 匹配所有路径
 };
 
+const CORRECT_PASSWORD = process.env.SITE_PASSWORD; // 使用环境变量
+
 export default function middleware(req) {
   const url = req.nextUrl.clone();
   const cookie = req.headers.get('cookie') || '';
-  const password = process.env.SITE_PASSWORD;
 
   // 检查是否访问受保护的页面且没有正确的密码 cookie
   if (
-    !cookie.includes(`site_password=${password}`)
+    !cookie.includes(`site_password=${CORRECT_PASSWORD}`)
   ) {
     url.pathname = '/login'; // 重定向到登录页面
     return NextResponse.redirect(url);
